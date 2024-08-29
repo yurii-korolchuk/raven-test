d1b3eaf52eefc2192739ca5b7c2548475105e0e0
yurii-korolchuk <yura.korolchuk82@gmail.com>
2024-08-29 15:31:20 +0300

    refactor: generic storage creator

--- src/data/storage/index.ts
+++ src/data/storage/index.ts
@@ -1,2 +1,21 @@
-export * from "./cartStateStorage";
-export * from "./selectedCurrencyStorage";
+import { CartState } from "../store/store";
+import { Currency } from "../types";
+
+export const createStorage = <Value>(key: string) => {
+  return {
+    set(value: Value): void {
+      localStorage.setItem(key, JSON.stringify(value));
+    },
+    get(): Value | null {
+      const item = localStorage.getItem(key);
+      if (item) {
+        return JSON.parse(item);
+      }
+
+      return null;
+    },
+  };
+};
+
+export const cartStateStorage = createStorage<CartState>("cart");
+export const selectedCurrencyStorage = createStorage<Currency>("currency");
